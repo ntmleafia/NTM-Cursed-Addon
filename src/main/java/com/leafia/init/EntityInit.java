ec02099c3dbd8e71a574fb6a0f70b8d116f7db26
abysschroma <121738096+abysschroma@users.noreply.github.com>
2025-10-26 07:03:47 +0900

    Current progress on EntityNukeFolkvangr 5

--- src/main/java/com/leafia/init/EntityInit.java
+++ src/main/java/com/leafia/init/EntityInit.java
@@ -1,6 +1,5 @@
 package com.leafia.init;
 
-import com.hbm.lib.RefStrings;
 import com.hbm.main.MainRegistry;
 import com.leafia.contents.effects.folkvangr.EntityNukeFolkvangr;
 import net.minecraft.util.ResourceLocation;
@@ -9,6 +8,6 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 public class EntityInit {
 	public static void preInit() {
 		int i = 0;
-		EntityRegistry.registerModEntity(new ResourceLocation(RefStrings.MODID, "entity_nuke_folkvangr"), EntityNukeFolkvangr.class, "entity_nuke_folkvangr", i++, MainRegistry.instance, 1000, 1, true);
+		EntityRegistry.registerModEntity(new ResourceLocation("leafia", "entity_nuke_folkvangr"), EntityNukeFolkvangr.class, "entity_nuke_folkvangr", i++, MainRegistry.instance, 1000, 1, true);
 	}
 }
--- src/main/java/com/leafia/overwrite_contents/mixin/mod/hbm/MixinEntityNukeExplosionMK3.java
+++ src/main/java/com/leafia/overwrite_contents/mixin/mod/hbm/MixinEntityNukeExplosionMK3.java
@@ -1,22 +1,50 @@
 package com.leafia.overwrite_contents.mixin.mod.hbm;
 
+import com.hbm.config.CompatibilityConfig;
 import com.hbm.entity.logic.EntityNukeExplosionMK3;
 import com.hbm.entity.logic.IChunkLoader;
+import com.hbm.items.ModItems;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = EntityNukeExplosionMK3.class)
 public abstract class MixinEntityNukeExplosionMK3 extends Entity implements IChunkLoader {
+	@Shadow(remap = false) public int extType;
+	@Shadow(remap = false) public boolean did;
+	@Shadow(remap = false) public boolean waste;
+
+	@Shadow(remap = false) public abstract void setDead();
+
 	public MixinEntityNukeExplosionMK3(World worldIn) {
 		super(worldIn);
 	}
 
 	@Inject(method = "onUpdate", at = @At("HEAD"))
 	private void onOnUpdate(CallbackInfo ci) {
-
+		if (!this.world.isRemote) {
+			if (CompatibilityConfig.isWarDim(this.world)) {
+				if (!this.did) {
+					if (!waste) {
+						if (extType == 0) {
+							System.out.println("TEST");
+							for (EntityPlayer player : world.playerEntities) {
+								if (player.getHeldItemMainhand().getItem() == ModItems.wand_d)
+									player.sendMessage(new TextComponentString("TEST").setStyle(new Style().setColor(TextFormatting.YELLOW)));
+							}
+							this.setDead();
+						}
+					}
+				}
+			}
+		}
 	}
 }
