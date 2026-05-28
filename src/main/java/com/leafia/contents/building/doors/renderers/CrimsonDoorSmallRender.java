4402cf195d29976255cf8b11cb01f87d0e90a999
ntmleafia <121738096+ntmleafia@users.noreply.github.com>
2026-05-28 22:01:40 +0900

    feat: clipping planes!

--- src/main/java/com/leafia/contents/building/doors/renderers/CrimsonDoorSmallRender.java
+++ src/main/java/com/leafia/contents/building/doors/renderers/CrimsonDoorSmallRender.java
@@ -9,6 +9,7 @@ import com.leafia.transformer.LeafiaGls;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 import java.nio.DoubleBuffer;
 
@@ -20,7 +21,7 @@ public class CrimsonDoorSmallRender implements IRenderDoors {
 	public static final ResourceLocation tex = getIntegrated("decoration/doors/crimdoorlarge/texture.png");
 	public static final WaveFrontObjectVAO vao = getVAO(getIntegrated("decoration/doors/crimdoorsmall/crimdoorsmall.obj"));
 	@Override
-	public void render(TileEntityDoorGeneric te,DoubleBuffer doubleBuffer) {
+	public void render(TileEntityDoorGeneric te,DoubleBuffer buf) {
 		LeafiaGls.translate(0,0,0);
 		double doorRatio = 0;
 		if (te.state == DoorState.OPEN)
@@ -28,6 +29,12 @@ public class CrimsonDoorSmallRender implements IRenderDoors {
 		if (te.currentAnimation != null)
 			doorRatio = IRenderDoors.getRelevantTransformation("DOOR",te.currentAnimation)[1];
 		Minecraft.getMinecraft().getTextureManager().bindTexture(tex);
+		GL11.glEnable(GL11.GL_CLIP_PLANE0);
+		buf.put(new double[] { 0.0, 0.0, 1, 1.499 }); buf.rewind();
+		GL11.glClipPlane(GL11.GL_CLIP_PLANE0, buf);
+		GL11.glEnable(GL11.GL_CLIP_PLANE1);
+		buf.put(new double[] { 0.0, 0.0, -1, 1.499 }); buf.rewind();
+		GL11.glClipPlane(GL11.GL_CLIP_PLANE1, buf);
 		{
 			LeafiaGls.pushMatrix();
 			LeafiaGls.translate(0,0,1.4375*doorRatio);
@@ -40,6 +47,8 @@ public class CrimsonDoorSmallRender implements IRenderDoors {
 			vao.renderPart("DoorRight");
 			LeafiaGls.popMatrix();
 		}
+		GL11.glDisable(GL11.GL_CLIP_PLANE0);
+		GL11.glDisable(GL11.GL_CLIP_PLANE1);
 		vao.renderPart("Track");
 		vao.renderPart("OuterFrame");
 		vao.renderPart("InnerFrame");
