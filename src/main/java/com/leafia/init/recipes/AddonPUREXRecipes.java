2f62036ffb70eddc0379dda08fd7241ef99efcb9
xlzv <96470093+xlzv@users.noreply.github.com>
2026-06-28 17:09:01 +0900

    fix purex recipe

--- src/main/java/com/leafia/init/recipes/AddonPUREXRecipes.java
+++ src/main/java/com/leafia/init/recipes/AddonPUREXRecipes.java
@@ -3,8 +3,8 @@ package com.leafia.init.recipes;
 import com.hbm.inventory.RecipesCommon.ComparableStack;
 import com.hbm.inventory.fluid.FluidStack;
 import com.hbm.inventory.fluid.Fluids;
+import com.hbm.inventory.recipes.PUREXRecipe;
 import com.hbm.inventory.recipes.PUREXRecipes;
-import com.hbm.inventory.recipes.loader.GenericRecipe;
 import com.hbm.items.ModItems;
 import com.leafia.contents.AddonItems;
 import com.leafia.contents.AddonItems.DepletedFuels;
@@ -20,7 +20,7 @@ public class AddonPUREXRecipes {
 		long vitrification = 1_000;
 		// ZIRNOX
 		String autoZirnox = "autoswitch.zirnox";
-		INSTANCE.register(new GenericRecipe("purex.leafia.zirnoxu238").setup(100,zirnoxPower).setNameWrapper("purex.recycle").setGroup(autoZirnox,INSTANCE)
+		INSTANCE.register((PUREXRecipe) new PUREXRecipe("purex.leafia.zirnoxu238").setup(100,zirnoxPower).setNameWrapper("purex.recycle").setGroup(autoZirnox,INSTANCE)
 				.inputItems(new ComparableStack(DepletedFuels.waste_u238))
 				.inputFluids(new FluidStack(Fluids.KEROSENE,500),new FluidStack(Fluids.NITRIC_ACID,250))
 				.outputItems(
